1921797a00c2428b7d92f22f94e65daa74e4a888
group6ipt123-cpu <group6.ipt123@gmail.com>
2026-05-25 09:23:12 +0800

    Update ProfileScreen.jsx

--- frontend/src/components/ProfileScreen.jsx
+++ frontend/src/components/ProfileScreen.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'https://lostandfound-ipt-1.onrender.com';
 
 const styles = `
   @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap');
