6731c1cc11f345be403a7cc0f930ebd2e2557283
group6ipt123-cpu <group6.ipt123@gmail.com>
2026-05-25 09:21:38 +0800

    Update AllPostsScreen.jsx

--- frontend/src/components/AllPostsScreen.jsx
+++ frontend/src/components/AllPostsScreen.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'https://lostandfound-ipt-1.onrender.com';
 
 const styles = `
   .all-posts-page {
